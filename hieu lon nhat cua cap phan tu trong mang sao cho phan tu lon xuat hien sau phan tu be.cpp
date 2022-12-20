3065cd2f30fb2743069694d097fec6a3372df800
nhtplusplus <112921014+leduongnhatcplusplus@users.noreply.github.com>
2022-12-20 09:53:36 +0700

    Add files via upload

---
+++ hieu lon nhat cua cap phan tu trong mang sao cho phan tu lon xuat hien sau phan tu be.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i;i<n;i++){
+		cin>>a[i];
+	}
+	int min_value=a[0];
+	int res=-1e9;
+	for(int i=1;i<n;i++){
+		if(a[i]>min_value){
+			res=max(res,a[i]-min_value);
+		}
+		min_value=min(min_value,a[i]);
+	}
+	if(res=-1e9){
+		cout<<"-1";
+		}
+		else cout<<res;
+}
